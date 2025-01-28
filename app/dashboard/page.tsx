cace948544711d933b8e0af0769c33ff9eb3d4ae
unknown <atte.marjamaki@arcada.fi>
2025-01-28 22:43:11 +0200

    added auth file

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,16 @@
 import Dashboard from "@/components/Dashboard";
+import db from "@/app/db";
+import { getServerSession } from "next-auth";
 
-export default function DashboardPage() {
+async function getbalance() {
+  const session = await getServerSession();
+
+  db.solWallet.findFirst({
+    where: {},
+  });
+}
+
+export default async function () {
   return (
     <div className="container max-w-5xl mx-auto p-2 pt-4 lg:pt-16">
       <Dashboard />
